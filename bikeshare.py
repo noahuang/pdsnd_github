8cf5b02e9a529658a8c2067c5574dab0a14bc1dc
Zhaohui_MacOS <zhaohui.huang@rwth-aachen.de>
2023-01-02 21:48:19 +0800

    202301022148 update #MONTH Dict from 0 to 12

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-#MONTH Dict
+#MONTH Dict from 0 to 12
 MONTH = {"all":0, "january":1, "february":2, "march":3 , "april":4 , "may":5 , "june":6 ,"july":7 , "august":8 , "september":9 , "october":10 , "november":11 , "december":12 }
 #DAY Dict
 DAY = {"all":7, "monday":1, "tuesday":2, "wednesday":3, "thursday":4 , "friday":5 , "saturday":6 , "sunday":0}
